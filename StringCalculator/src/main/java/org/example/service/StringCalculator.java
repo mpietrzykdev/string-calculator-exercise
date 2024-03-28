a0b70a5d28bed8e54feb97b3eb0ac65f567129ec
Martyna Pietrzyk <mpietrzyk.dev@gmail.com>
2024-03-28 08:27:11 +0100

    Refactor code to improve readability and maintainability - split 'add' method into smaller private methods

--- StringCalculator/src/main/java/org/example/service/StringCalculator.java
+++ StringCalculator/src/main/java/org/example/service/StringCalculator.java
@@ -3,16 +3,28 @@ package org.example.service;
 public class StringCalculator {
 
     public static int add(String numbers) {
-        if (numbers.isEmpty()) {
+        if (isEmpty(numbers)) {
             return 0;
         }
 
-        char lastChar = numbers.charAt(numbers.length() - 1);
+        checkLastCharacter(numbers);
+
+        String[] numbersArray = numbers.split("[,\n]");
+        return sumNumbers(numbersArray);
+    }
+
+    private static boolean isEmpty(String input) {
+        return input.isEmpty();
+    }
+
+    private static void checkLastCharacter(String input) {
+        char lastChar = input.charAt(input.length() - 1);
         if (lastChar == ',' || lastChar == '\n') {
             throw new IllegalArgumentException("Input cannot end with a separator.");
         }
+    }
 
-        String[] numbersArray = numbers.split("[,\n]");
+    private static int sumNumbers(String[] numbersArray) {
         int sum = 0;
         for (String num : numbersArray) {
             try {
